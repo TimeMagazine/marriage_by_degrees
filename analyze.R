f591620c95789664ff192f0ae399445519975630
Christopher Wilson <christopher.e.wilson@gmail.com>
2016-09-16 03:52:18 -0400

    finished filtering

--- analyze.R
+++ analyze.R
@@ -1,6 +1,8 @@
 # Code for degree compability
 # Link TK
 # By Chris Wilson
+# LICENSE: Creative Commons Attribution 3.0
+# https://creativecommons.org/licenses/by/3.0/us/
 # DISCLAIMER: I am not a pro at R, so all calculations here were
 # tested against manual SQL queries of the same dataset
 # Please forgive sloppy code, or better yet, correct it with 
@@ -37,25 +39,45 @@ for (detailed in degrees$detailed) {
 }
 
 # every couple is in the data twice, once as the primary and once as the spouse
-# let's confirm that we have the same number of degrees in both DEGSIMPLE AND DEGSIMPLE_SP
+# let's confirm that we have the same number of degrees
+# in both DEGSIMPLE AND DEGSIMPLE_SP
 table(two_degrees$DEGSIMPLE) == table(two_degrees$DEGSIMPLE_SP)
 
-# Let's look at overall popularity of degrees in spouses
-# we're going to look at the raw record count and the
-# weighted count
+# Let's look at overall popularity of degrees in primaries and spouses
+# we're going to look at the raw record count and the weighted count
+count    <- aggregate(two_degrees$PERWT, by=list("degree" = two_degrees$DEGSIMPLE), FUN = length)
+weighted <- aggregate(two_degrees$PERWT, by=list("degree" = two_degrees$DEGSIMPLE), FUN = sum)
+
+# let's combine into one data frame
+colnames(count) <- c("degree","count_degree")
+colnames(weighted) <- c("degree","weighted_degree")
+popularity <- merge(count, weighted, by=c("degree"))
+
+# and do the same for spouses
 count    <- aggregate(two_degrees$PERWT, by=list("degree_spouse" = two_degrees$DEGSIMPLE_SP), FUN = length)
 weighted <- aggregate(two_degrees$PERWT, by=list("degree_spouse" = two_degrees$DEGSIMPLE_SP), FUN = sum)
 
-# let's combine into one data frame
-colnames(count) <- c("degree_spouse","count")
-colnames(weighted) <- c("degree_spouse","weighted")
-popularity <- merge(count, weighted, by=c("degree_spouse"))
+# again, let's combine into one data frame
+colnames(count) <- c("degree_spouse","count_spouse")
+colnames(weighted) <- c("degree_spouse","weighted_spouse")
+popularity_spouse <- merge(count, weighted, by=c("degree_spouse"))
+
+# and merge into one data frame
+popularity <- merge(popularity, popularity_spouse, by.x=c("degree"), by.y=c("degree_spouse"))
 
 # and measure the percent of spouses with a given degree from 0-100
-popularity$percent <- popularity$weighted / population * 100
+popularity$percent <- popularity$weighted_degree / population * 100
+popularity$percent_spouse <- popularity$weighted_spouse / population * 100
 
-# few, this sums to 100
+# phew, these sum to 100
 sum(popularity$percent)
+sum(popularity$percent_spouse)
+
+#NOTE
+# You may notice that the percentages for percent and percent_spouse
+# do no perfectly match, even though everyone is in the data twice
+# This is because a person and his or her spouse do not have exactly
+# the same weight. We expect this. But the values are VERY close
 
 # now let's do the same thing for every combination of major
 count    <- aggregate(two_degrees$PERWT, by=list("degree" = two_degrees$DEGSIMPLE, "degree_spouse" = two_degrees$DEGSIMPLE_SP), FUN = length)
@@ -69,20 +91,22 @@ matches <- merge(count, weighted, by=c("degree", "degree_spouse"))
 # okay, now we need to find the RELATIVE popularity of a degree 
 # in a spouse compared to that of the general population
 popularity_by_degree_combo <- function(match) {
-  overall_popularity = popularity$weighted[popularity$degree_spouse==match[["degree_spouse"]]]
+  # how popular is the primary degree?
+  number_of_primary_majors = popularity$weighted_degree[popularity$degree==match[["degree"]]]
   specific_popularity = as.numeric(match[["weighted"]]);
-  
-  # and what percent of people with the spouses degree married someone with the 
-  # primary's degree?
-  return(100 * specific_popularity / overall_popularity);
+    
+  # and what percent of people with the spouse's degree married someone
+  # with the primary's degree?
+  return(100 * specific_popularity / number_of_primary_majors);
 }
 
 matches$percent <- apply(matches, 1, popularity_by_degree_combo)
 
 # and compare that to the overall percent to get the relative odds
 odds_by_degree_combo <- function(match) {
-  overall_percent = popularity$percent[popularity$degree_spouse==match[["degree"]]]
+  overall_percent = popularity$percent_spouse[popularity$degree==match[["degree_spouse"]]]
   specific_percent = as.numeric(match[["percent"]]);
+  print(paste(match[["degree"]], match[["degree_spouse"]], match[["degree_spouse"]], overall_percent, specific_percent));
   
   # and what percent of people with the spouses degree married someone with the 
   # primary's degree?
@@ -91,7 +115,14 @@ odds_by_degree_combo <- function(match) {
 
 matches$odds <- apply(matches, 1, odds_by_degree_combo)
 
+# NOTE
+# Again, because of different weights, the compatibilities
+# are not perfectly reciprocal, but very close
+
 # We only want combos with at least 30 examples
 filtered = subset(matches, matches$count >= 30);
+
+
+
 
 write.csv(filtered, "filtered.csv")
